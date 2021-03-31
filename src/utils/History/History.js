df946e5a354ca1066788182a3dfcc65702904d3e
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2021-03-30 18:23:17 -0600

    Remove Id History

--- src/utils/History/History.js
+++ src/utils/History/History.js
@@ -1,18 +1,13 @@
-import Factor from "../Factors/Factor";
+import Factor from '../Factors/Factor';
 
 export default class History extends Factor {
-  constructor(factor, period, interestRate, result, date, id = undefined) {
-    super(factor, period, interestRate);
-    this.setResult(result);
-    this.id = typeof id == "undefined" ? Date.now() : id;
-    this.date = date;
-  }
+	constructor(factor, period, interestRate, result, date, id = undefined) {
+		super(factor, period, interestRate);
+		this.setResult(result);
+		this.date = date;
+	}
 
-  getId() {
-    return this.id;
-  }
-
-  getDate() {
-    return this.date;
-  }
+	getDate() {
+		return this.date;
+	}
 }
