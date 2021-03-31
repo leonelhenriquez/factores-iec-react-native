85ffe48112c3d7129656cddc3652371c6c97d9eb
Leonel Henriquez <leonelhenriquez@users.noreply.github.com>
2021-03-30 20:25:24 -0600

    Revert "Dev"

--- src/Settings.js
+++
@@ -1,7 +0,0 @@
-const Settings = {
-	decimal_precision: 5,
-	version: '1.2',
-	maxHistory: 50,
-};
-
-export default Settings;
--- src/utils/isNumber.js
+++
@@ -1,7 +0,0 @@
-import NumberResolver from './NumberResolver';
-
-const isNumber = strNumber => {
-	return !Number.isNaN(NumberResolver(strNumber));
-};
-
-export default isNumber;
